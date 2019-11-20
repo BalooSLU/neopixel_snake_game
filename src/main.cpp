80f9c91f241636780f1020ca74990ec942eff2c4
BalooSLU <prinzslu@gmail.com>
2019-11-20 09:33:52 +0100

    Included some usefull headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <FS.h>   //Include File System Headers
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ESP8266mDNS.h>
 
 void setup() {
   // put your setup code here, to run once:
